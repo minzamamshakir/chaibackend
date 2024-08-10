c1e259fd6c01e3a0f23a2db3e14cc9e615cdabde
inzamam.535 <aidigitaldreamers@gmail.com>
2024-08-10 17:15:35 +0500

    Server running simple Post Request for Registration

--- src/app.js
+++ src/app.js
@@ -10,4 +10,9 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Router Importing
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/user", userRouter);
+
 export { app };
